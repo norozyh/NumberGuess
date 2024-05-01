8f22d8fa0844c12a77f788c7f5e19ae5e6725500
norozyh zheng <norozyh@163.com>
2024-05-01 01:45:34 +0000

    feat:check user

--- number_guess.sh
+++ number_guess.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=guess_number -t --no-align -c"
 SECRET=$(( $RANDOM % 1000 +1 ))
 echo $SECRET
 echo Enter your username:
 read USERNAME
 
  #检查db中是否有user
+PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE name='$USERNAME'")
+if [[ -z $PLAYER_ID ]]
 
-if [[ $1 ]]
-
-#若有，欢迎并显示历史战绩
-then
-  echo Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses.
 #若无，欢迎新用户
+then
+  echo Welcome, $USERNAME! It looks like this is your first time here.
+  INSERT_USER=$($PSQL "INSERT INTO players(name) VALUES('$USERNAME');")
+#若有，欢迎并显示历史战绩
 else
-  echo Welcome, <username>! It looks like this is your first time here.
-
+  RESULT=$($PSQL "SELECT player_id,games_played,best_game FROM players WHERE name='$USERNAME';")
+  echo $RESULT
+  echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
 FLAG=0
+CNT=0
 while [[ $FLAG = 0 ]]
 do
   echo Guess the secret number between 1 and 1000:
   read GUESS
-  if [[ !$GUESS =~ [0~9]+ ]]
+  CNT=$(( $CNT+1 ))
+  if ! [[ $GUESS =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
   elif [[ $GUESS > $SECRET ]]
@@ -32,7 +37,8 @@ do
   then
     echo "It's higher than that, guess again:"
   else
-    echo You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!
+    echo You guessed it in $CNT tries. The secret number was $SECRET. Nice job!
     FLAG=1
+    #db中存入记录
   fi
 done
